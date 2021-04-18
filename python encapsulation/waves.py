f6d1378f30f78676c9cbce3f90a04cf27dd2102d
Jefferson-Lopes <jeff.777.lopes@gmail.com>
2021-04-18 12:40:41 -0300

    Update waves.py

--- python encapsulation/waves.py
+++ python encapsulation/waves.py
@@ -1,15 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-time = np.arange(0, 32, 0.5)
-wave = 256*np.sin(time)   #max amplitude: 512 
-wave = wave.astype(int)   #convert to int
-wave = wave + 256         #add offset 
+FREQ = 5
+AMP  = 512
+TIME = np.arange(0, 8, .125)       #64 points
+wave = AMP/2*np.sin(FREQ * TIME)   #max amplitude: 512 
+wave = wave.astype(int)            #convert to int
+wave = wave + AMP/2                #add offset 
 
 di_re = wave
 di_im = np.zeros(64).astype(int)
 
-plt.plot(time, di_re)
-plt.plot(time, di_im)
+plt.figure(1)
 plt.title('Sine wave')
+plt.plot(TIME, di_re, label='Real')
+plt.plot(TIME, di_im, label='Imag')
+plt.legend()
 plt.show()
