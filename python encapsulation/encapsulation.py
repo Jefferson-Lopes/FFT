dcadb0e1b1c6e69738223599df5f75e56dce4e27
Jefferson-Lopes <jeff.777.lopes@gmail.com>
2021-04-27 10:43:22 -0300

    new communication standard

--- python encapsulation/encapsulation.py
+++ python encapsulation/encapsulation.py
@@ -9,9 +9,8 @@ AMP  = 512
 TIME = np.arange(0, 8, .125)       #64 points
 wave = AMP/2*np.sin(FREQ * TIME)   #max amplitude: 512 
 wave = wave + AMP/2                #add offset 
-wave = wave.astype(int)            #convert to int
 
-di_re = wave
+di_re = wave.astype(int)
 di_im = np.zeros(64).astype(int)
 
 
@@ -23,31 +22,35 @@ file_wr = open('R22SDF/simulation/modelsim/input.txt', 'w')
 #second file for keep track more easily
 file_wr_clone = open('python encapsulation/input_clone.txt', 'w')
 
-#write the column labels
-file_wr.write('real,imag')
-file_wr_clone.write('real,imag')
-
 #write the waves
 for i in range(64):
-    text = '\n' + str(di_re[i]) + ',' + str(di_im[i])
-    file_wr.write(text)
-    file_wr_clone.write(text)
+    #real
+    file_wr.write(str(di_re[i]) + '\n')
+    file_wr_clone.write(str(di_re[i]) + '\n')
+
+    #imag
+    file_wr.write(str(di_im[i]) + '\n')
+    file_wr_clone.write(str(di_im[i]) + '\n')
 
 #close both files
 file_wr.close()
 file_wr_clone.close()
 
 
+
 ###########################################
 ## process the data in verilog testbench ##
 ###########################################
 #using make_output for testing
+print('The input file was generated. ')
+print('Please run the Verilog testbench then press enter.')
+input('Press enter. . .')
 
 
 #################################
 ## process the data with NumPy ##
 #################################
-comp = di_re + di_im*1j    #convert to complex list
+comp = di_re + di_im*1j    #convert to complex array
 
 fft_np = np.fft.fft(comp)
 
@@ -61,12 +64,12 @@ file_rd = open('python encapsulation/output_clone.txt', 'r')
 data_read = file_rd.read()
 file_rd.close()
 data_read = data_read.split("\n")
-data_read = [i.split(",") for i in data_read]
-data_real = np.array([row[0] for row in data_read])
-data_imag = np.array([row[1] for row in data_read])
+data_read.pop() #delete the last element: '\n'
+data_real = np.array(data_read[0::2])
+data_imag = np.array(data_read[1::2])
 
-do_re = data_real[1:].astype(int)
-do_im = data_imag[1:].astype(int)
+do_re = data_real.astype(int)
+do_im = data_imag.astype(int)
 
 fft_fpga = do_re + do_im*1j
 
@@ -74,6 +77,7 @@ fft_fpga = do_re + do_im*1j
 ###################################################
 ## plot the result and the same thing with NumPy ##
 ###################################################
+print('Plotting...')
 plt.figure(1)
 plt.title('Input wave')
 plt.plot(TIME, di_re, label='Real')
@@ -84,18 +88,14 @@ plt.savefig('python encapsulation/input.png', bbox_inches='tight')
 
 plt.figure(2)
 plt.title('NumPy FFT')
-plt.plot(TIME,    fft_np.real, label='Real')
-plt.plot(TIME,    fft_np.imag, label='Imag')
-plt.plot(TIME, np.abs(fft_np),  label='ABS')
+plt.plot(TIME, np.abs(fft_np), label='ABS')
 plt.legend()
 plt.grid()
 plt.savefig('python encapsulation/fft_np.png', bbox_inches='tight')
 
 plt.figure(3)
 plt.title('FPGA FFT')
-plt.plot(TIME,    fft_fpga.real, label='Real')
-plt.plot(TIME,    fft_fpga.imag, label='Imag')
-plt.plot(TIME, np.abs(fft_fpga),  label='ABS')
+plt.plot(TIME, np.abs(fft_fpga), label='ABS')
 plt.legend()
 plt.grid()
 plt.savefig('python encapsulation/fft_fpga.png', bbox_inches='tight')
--- python encapsulation/make_output.py
+++ python encapsulation/make_output.py
@@ -5,12 +5,13 @@ file1 = open('python encapsulation/input_clone.txt', 'r')
 data_read = file1.read()
 file1.close()
 data_read = data_read.split("\n")
-data_read = [i.split(",") for i in data_read]
-data_real = np.array([row[0] for row in data_read])
-data_imag = np.array([row[1] for row in data_read])
+data_read.pop() #delete the last element: '\n'
+data_real = np.array(data_read[0::2])
+data_imag = np.array(data_read[1::2])
 
-di_re = data_real[1:].astype(int)
-di_im = data_imag[1:].astype(int)
+di_re = data_real.astype(int)
+di_im = data_imag.astype(int)
+
 TIME = np.arange(0, 8, .125)       #64 points
 
 comp = di_re + di_im*1j
@@ -18,8 +19,12 @@ comp = di_re + di_im*1j
 fft = np.fft.fft(comp)
 
 file2 = open('python encapsulation/output_clone.txt', 'w')
-file2.write('real,imag')
 for i in range(64):
-    text = '\n' + str(int(fft[i].real)) + ',' + str(int(fft[i].imag))
-    file2.write(text)
+    #real
+    file2.write(str(int(fft[i].real)) + '\n')
+    #imag
+    file2.write(str(int(fft[i].imag)) + '\n')
+
 file2.close()
+
+print('File generated successfully!')
